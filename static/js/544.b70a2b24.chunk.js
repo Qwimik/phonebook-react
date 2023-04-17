4a060a50b67ab94cbb3c62421febad3cc1251b8b
Qwimik <109024305+Qwimik@users.noreply.github.com>
2023-04-17 20:33:56 +0000

    Deploying to gh-pages from @ Qwimik/goit-react-hw-08-phonebook@35e76a3e2788756512b141b68cfe270e748a1875 🚀

--- static/js/544.b70a2b24.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[544],{9544:function(e,n,o){o.r(n),o.d(n,{default:function(){return r}});var t=o(184);function r(){return(0,t.jsx)("div",{children:"Home"})}}}]);
-//# sourceMappingURL=544.b70a2b24.chunk.js.map
